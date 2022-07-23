c01763c41d8a580543867e4c75adbfe5f75e3f5c
alaqsaka <aqsaabidin15@gmail.com>
2022-07-23 22:51:42 +0700

    feat: slicing task card, create new task button

---
+++ src/components/atoms/Task/index.js
@@ -0,0 +1,76 @@
+import {
+  Box,
+  Divider,
+  Flex,
+  IconButton,
+  Progress,
+  Spacer,
+  Text,
+} from '@chakra-ui/react';
+import React from 'react';
+import { BsThreeDots } from 'react-icons/bs';
+
+const Task = () => {
+  return (
+    <Box
+      bg="rgba(250, 250, 250, 1)"
+      borderWidth="1px"
+      style={{
+        padding: '16px',
+        borderRadius: '4px',
+        border: '1px solid rgba(224, 224, 224, 1)',
+      }}
+    >
+      <Text color="rgba(64, 64, 64, 1)" style={{ fontWeight: '700' }}>
+        Re-designed the zero-g doggie bags. No more spills!
+      </Text>
+
+      <Divider
+        variant="dashed"
+        style={{
+          color: 'rgba(224, 224, 224, 1)',
+          marginTop: '8px',
+          marginBottom: '12px',
+        }}
+        colorScheme="rgba(224, 224, 224, 1)"
+      />
+
+      <Flex gap={2}>
+        <Flex gap={2} alignItems="center">
+          <Box w="188px">
+            {/* Warna dari progres kalo belum selesai biru, tapi kalo udah selesai (100%) jadi warna ijo */}
+            <Progress
+              value={30}
+              style={{ borderRadius: '9999px', height: '16px' }}
+            />
+          </Box>
+
+          <Box>
+            {/* Angka progres diganti logo check kalau sudah 100% */}
+            <p
+              style={{
+                fontSize: '12px',
+                lineHeight: '16px',
+                color: 'rgba(117, 117, 117, 1)',
+                fontWeight: '400',
+              }}
+            >
+              30%
+            </p>
+          </Box>
+        </Flex>
+        <Spacer />
+        <Box>
+          <IconButton
+            bg="none"
+            colorScheme="rgba(237, 237, 237, 1);"
+            icon={<BsThreeDots color="rgba(117, 117, 117, 1) " />}
+            fontSize="24px"
+          />
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Task;
--- src/components/molecules/GroupTaskCard/index.js
+++ src/components/molecules/GroupTaskCard/index.js
@@ -1,7 +1,11 @@
-import { Badge, Box } from '@chakra-ui/react';
+import { Badge, Box, Button, useDisclosure } from '@chakra-ui/react';
 import React from 'react';
+import Task from '../../atoms/Task';
+import { BsPlusCircle } from 'react-icons/bs';
+import CreateTaskModal from '../../atoms/CreateTaskModal';
 
 const GroupTaskCard = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   // Style
   // Accepting props for background color
   // Accepting props for border color
@@ -46,6 +50,32 @@ const GroupTaskCard = () => {
       >
         January - March
       </p>
+
+      {/* Tasks */}
+      <div>
+        <Task />
+      </div>
+
+      {/* New Task Button */}
+      <Button
+        onClick={onOpen}
+        bg="none"
+        colorScheme="none"
+        color="rgba(29, 31, 32, 1)"
+        style={{
+          fontSize: '12px',
+          fontWeight: '400',
+          lineHeight: '20px',
+          padding: '0px',
+        }}
+        leftIcon={
+          <BsPlusCircle color="rgba(51, 51, 51, 1)" fontSize="16.67px" />
+        }
+      >
+        New Task
+      </Button>
+
+      <CreateTaskModal onOpen={onOpen} isOpen={isOpen} onClose={onClose} />
     </Box>
   );
 };
